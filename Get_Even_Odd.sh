3926a1adc4786a2f29652794ce2da798620404a0
iakhil31 <akhil.iam3113@gmail.com>
2023-12-17 11:43:41 -0600

    changes made to even or odd file

--- Get_Even_Odd.sh
+++ Get_Even_Odd.sh
@@ -1,5 +1,7 @@
 #Scripting for Even & Odd Numbrs.
  #!/bin/bash
+ for I in {1..5}
+ do
  read -p "Please Enter A valid number: " NUM
  echo $NUM
  if [ $(expr $NUM % 2) -eq 0 ]
@@ -7,4 +9,5 @@
  echo "NUM is a Even Number"
  else
  echo "NUM is a Odd Number"
- fi 
+ fi
+ done 
