a16ae6013b55b91c9da3a935ab0dc2c3a2296bc6
iakhil31 <akhil.iam3113@gmail.com>
2023-12-17 18:23:48 -0600

    User automation file

---
+++ User_Automation.sh
@@ -0,0 +1,17 @@
+#This Script does the following
+# - Take username as input argument
+# - Check if the username already exists.
+# - Prompt for a new Username if username already exists
+# - Generate Random password with special characters and Number. Eg: India@34344!
+# - Provide user sudo access
+# - Force user to reset the password on first login
+
+#!/bin/bash
+
+USERNAME=$1
+EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
+echo "The User "${USERNAME}" Already Exists."
+else
+echo "Lets Create The user "${USERNAME}". "|
+fi
