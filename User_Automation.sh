7735dff508af86e5affdfe7069bf169a2a0eab92
root <root@ip-10-38-1-155.ec2.internal>
2023-12-18 00:33:12 +0000

    Modified User_Automation file

--- User_Automation.sh
+++ User_Automation.sh
@@ -7,11 +7,14 @@
 # - Force user to reset the password on first login
 
 #!/bin/bash
-
+if [ $# -gt 0 ]; then
 USERNAME=$1
 EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
 if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
 echo "The User "${USERNAME}" Already Exists."
 else
-echo "Lets Create The user "${USERNAME}". "|
+echo "Lets Create The user "${USERNAME}"."|
+fi
+else
+	echo "Provide Valid Argument."
 fi
