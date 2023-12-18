85c927e76c2c2a47b52720d5bead1505f02c0f27
iakhil31 <akhil.iam3113@gmail.com>
2023-12-17 19:12:10 -0600

    Updates User_ Automationfile

--- User_Automation.sh
+++ User_Automation.sh
@@ -8,7 +8,6 @@
 
 #!/bin/bash
 if [ $# -gt 0 ]; then
-<<<<<<< HEAD
     USERNAME=$1
     EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
     if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
@@ -26,15 +25,3 @@ else
 else
      echo "Provide Valid Argument."
 fi
-=======
-USERNAME=$1
-EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
-if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
-echo "The User "${USERNAME}" Already Exists."
-else
-echo "Lets Create The user "${USERNAME}"."|
-fi
-else
-	echo "Provide Valid Argument."
-fi
->>>>>>> 7735dff508af86e5affdfe7069bf169a2a0eab92
