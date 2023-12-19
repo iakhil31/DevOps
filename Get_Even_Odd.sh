23aae75b91b48add00fed7c64685866e86e341e5
iakhil31 <akhil.iam3113@gmail.com>
2023-12-18 19:14:22 -0600

    Committed

--- Get_Even_Odd.sh
+++ Get_Even_Odd.sh
@@ -1,9 +1,7 @@
 #Scripting for Even & Odd Numbrs.
  #!/bin/bash
- #for I in {1..5}
- While true;
- do
- read -p "Please Enter A valid number: " NUM
+ for p in {1..5}
+ read p "Please Enter A valid number: " NUM
  echo $NUM
  if [ $(expr $NUM % 2) -eq 0 ]
  then
@@ -11,5 +9,5 @@
  else
  echo "NUM is a Odd Number"
  fi
- done 
+  
 
