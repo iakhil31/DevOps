ff912eb12437bcf17e8c071965eacc696dd170f9
iakhil31 <akhil.iam3113@gmail.com>
2023-12-18 18:50:27 -0600

    Modified User_Automation File

--- User_Automation.sh
+++ User_Automation.sh
@@ -16,11 +16,12 @@ else
         echo "Lets Create The user "${USERNAME}"." 
         sudo useradd -m ${USERNAME} --shell /bin/bash 
         sudo usermod -aG sudo ${USERNAME}
-        echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
-        SPEC=$(echo ' 1@#$%*&*()_' | fold -wl | shuf | head -1)
+        echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL'  >>/etc/sudoers
+        SPEC=$(echo ' !@#$%^&*()_' | fold -w1 | shuf | head -1)
         PASSWORD="India@${RANDOM}${SPEC}"
         echo "${USERNAME):${PASSWORD}" | sudo chpasswd
         passwd -e ${USERNAME}
+        echo "The Temporary Credentails are ${USERNAME} and ${PASSWORD}"
    fi 
 else
      echo "Provide Valid Argument."
